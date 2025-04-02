bf125c19b610ad57a811bca6673a030c25915eba
janvi-chhayani <chhayanijanvi2003@gmail.com>
2025-04-02 15:23:36 +0530

    initial commit

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img className='logo' src="" alt="" />
+        <img src="" alt="" />
+      
+    </div>
+  )
+}
+
+export default Navbar
